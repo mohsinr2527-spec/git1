f7bac757eab1821fbb16e19dbe1e17b3ad3419a6
mohsinr2527 <mohsinr2527@gmail.com>
2025-10-30 19:19:48 +0530

    javascript file deleted

--- javascriptfile.js
+++
@@ -1,7 +0,0 @@
-this is simple file,text is ok
-ls
-sampletextfile1.txt
-clear
-[D[D[5~[5~[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D$clear
-git config clear
-[A[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[A[A[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cthis is sample text file,text is ok
